14a79ad094a9c8b0e6318f769c61b73df07dacdc
Junior Obando <juniorov@gmail.com>
2025-10-25 01:09:11 -0600

    add middleware

--- backend/src/routes/requests.routes.js
+++ backend/src/routes/requests.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { RequestsController } from "../controllers/requests.controller.js";
+import { validateDNI } from "../middlewares/vadalite.middleware.js";
 
 const router = Router();
 
-router.get('/request/:dni', RequestsController.getById);
+router.get('/request/:dni', validateDNI, RequestsController.getById);
 // router.post('/consult-and-save', RequestsController.consultAndSave);
 
 export default router;
