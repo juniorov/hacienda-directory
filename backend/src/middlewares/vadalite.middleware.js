bb6e5139698231819cc6209eefebccf81b160808
Junior Obando <juniorov@gmail.com>
2025-10-25 01:09:19 -0600

    remove console log

--- backend/src/middlewares/vadalite.middleware.js
+++ backend/src/middlewares/vadalite.middleware.js
@@ -1,8 +1,6 @@
 import {validationResult, body, param} from 'express-validator';
 
 export const validateResult = (req, res, next) => {
-    console.log(req.params.dni.length);
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ success: false, errors: errors.array() });
